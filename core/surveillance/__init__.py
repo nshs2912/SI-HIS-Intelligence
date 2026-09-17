d78e7b2aa6d0e6dbe39454d67c613800cf041f5f
nshs2912 <komunitassolfeggio@gmail.com>
2026-09-17 22:54:11 +0700

    feat: expose regulatory KLB evaluator

--- core/surveillance/__init__.py
+++ core/surveillance/__init__.py
@@ -1,5 +1,6 @@
 """Surveillance and early-warning layer."""
 from .ews import early_warning
 from .waves import detect_waves
+from .klb import evaluate_klb
 
-__all__ = ["early_warning", "detect_waves"]
+__all__ = ["early_warning", "detect_waves", "evaluate_klb"]

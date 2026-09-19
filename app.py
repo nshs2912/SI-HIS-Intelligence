3b674481158e4cb8a1ca466cb0ade3775183ac5d
nshs2912 <komunitassolfeggio@gmail.com>
2026-09-19 07:49:09 +0700

    Fix escaped newline in app imports

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from core.narrative import descriptive_expert, epidemiology_expert, curve_expert, spatial_expert, risk_expert, ml_expert, ai_prediction_expert, vulnerable_expert\nimport warnings
+from core.narrative import descriptive_expert, epidemiology_expert, curve_expert, spatial_expert, risk_expert, ml_expert, ai_prediction_expert, vulnerable_expert
+import warnings
 import folium
 import numpy as np
 import pandas as pd
